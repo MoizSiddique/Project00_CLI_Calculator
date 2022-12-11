8cbdda658aed34524efa3583c5671e733e5ae3db
MoizSiddique <moizsiddique27@gmail.com>
2022-12-11 13:26:47 +0500

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+
+enum Operators {
+    Addition = "addition",
+    Subtraction = "subtraction",
+    Multiplication = "multiplication",
+    Division = "divison"
+}
+
+async function userPrompt() {
+    let operator = await inquirer.prompt([{
+        name: "operation",
+        type: "list",
+        message: "Select Operator",
+        choices: Object.values(Operators)
+    }]);
+
+    let answers = await inquirer.prompt([{
+        name:"operand1",
+        type:"number",
+        message:"Enter first value:"
+    },
+    {
+        name:"operad2",
+        type:"number",
+        message:"Enter second value:"
+    }])
+
+    calculation(operator.operation, answers.operand1, answers.operad2);
+}
+
+function calculation(operator:string, operand1:number, operand2:number): void{
+    
+    let result = 0;
+    
+    switch(operator){
+        case "addition":
+            result = operand1 + operand2;
+            break;
+        case "subtraction":
+            result = operand1 - operand2;
+            break;
+        case "multiplication":
+            result = operand1 * operand2;
+            break;
+        case "Division":
+            result = operand1 / operand2;
+            break;
+    }
+
+    console.log(`The result of ${operator} ${operand1} and ${operand2}: ${result}`);
+}
+
+userPrompt();
+
